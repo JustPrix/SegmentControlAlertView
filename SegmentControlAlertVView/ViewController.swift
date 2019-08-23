25f8d24a80720c6dbb4878609ebee6d593aa4b07
COE-01 <coe-01@coe-01s-imac.local>
2019-08-23 15:32:17 +0530

    Initial Commit

---
+++ SegmentControlAlertVView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegmentControlAlertVView
+//
+//  Created by COE-01 on 23/08/19.
+//  Copyright © 2019 COE-01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
